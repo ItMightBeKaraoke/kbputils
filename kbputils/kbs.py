24cdb4a85f91bbd3b7e3dbe9890cd5334fe91fb7
ItMightBeKaraoke <code@itmightbekaraoke.com>
2024-04-11 00:41:30 -0500

    Only need one item from data_studio.ini, simplified code
    It turns out setremovalfx is used during reset and does change the project file, so really all that's needed is setoffset.

--- kbputils/kbs.py
+++ kbputils/kbs.py
@@ -1,45 +1,24 @@
 import os
+import sys
 
-# Metaclass to allow e.g. KBSSettings.offset instead of needing
-# KBSSettings().offset for the first invocation
 
-class ClassGetattr(type):
-    def __getattr__(cls, attr):
-        return cls.__getattr__(attr)
+def _fetch_offset():
+    if os.environ.get('APPDATA') and os.path.exists(p := os.path.join(os.environ['APPDATA'], 'Karaoke Builder', 'data_studio.ini')):
+        try:
+            with open(p, 'r') as f:
+                for line in f:
+                    field, val = line.split(maxsplit=1)
+                    if field == "setoffset":
+                        return int(val)
+        except OSError:
+            pass
+    return 0
 
-# Sort of a hacky singleton-like thing
-# It only reads the KBS config file if one of the settings is requested from
-# it, and fills in all settings.
+# Lazy-load the offset attribute so the file is only read if needed
 
-class KBSSettings(metaclass=ClassGetattr):
-    SETTING_NAMES = { 
-        "offset": "setoffset",
-        "removaltype": "setremovalfx",
-    }
-
-    _settings=None
-
-    @staticmethod
-    def _fetch_settings():
-        settings = {
-            "setoffset": 0,      # Offset to shift entire tracks
-            "setremovalfx": 3,   # Default removal style (3 is line by line)
-        }
-        if os.environ.get('APPDATA') and os.path.exists(p := os.path.join(os.environ['APPDATA'], 'Karaoke Builder', 'data_studio.ini')):
-            try:
-                with open(p, 'r') as f:
-                    for line in f:
-                        field, val = line.split(maxsplit=1)
-                        if field in settings:
-                            settings[field] = int(val)
-            except OSError:
-                pass
-        return settings
-
-    @classmethod
-    def __getattr__(cls, attr):
-        if not cls._settings:
-            cls._settings = cls._fetch_settings()
-            for x in cls.SETTING_NAMES:
-                setattr(cls, x, cls._settings[cls.SETTING_NAMES[x]])
-        return getattr(cls, attr)
+def __getattr__(attr):
+    if attr == "offset":
+        setattr(sys.modules[__name__], attr, _fetch_offset())
+        return getattr(sys.modules[__name__], attr)
+    else:
+        raise AttributeError(f"Module {__name__!r} has no attribute {attr!r}")
