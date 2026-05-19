f789af0bb1e0127b3560fa7b812a3b4094c66bd9
Change This <changethis@example.com>
2026-05-19 00:59:41 -0500

    Check for modified ass module earlier in init

--- src/kbputils/__init__.py
+++ src/kbputils/__init__.py
@@ -4,14 +4,15 @@ __version__ = '0.2.11'
 
 __all__ = ['KBPFile', 'AssConverter', 'DoblonTxtConverter', 'LRCConverter', 'KBPAction', 'KBPActionType', 'KBPTimingTarget', 'KBPTimingAnchor', 'KBPActionParams']
 
+import ass
+if not hasattr(ass.ScriptInfoSection, "add_comment"):
+    raise NotImplementedError("This version of kbputils is not compatible with the standard 'ass' module. Please uninstall 'ass' and install 'ass_imbk'. If both are installed, uninstall both, then reinstall the latter.")
+del ass
+
 from .kbp import *
 from .doblontxt import *
 from .lrc import *
 from .converters import *
-
-import ass
-if not hasattr(ass.ScriptInfoSection, "add_comment"):
-    raise NotImplementedError("This version of kbputils is not compatible with the standard 'ass' module. Please uninstall 'ass' and install 'ass_imbk'. If both are installed, uninstall both, then reinstall the latter.")
 
 if ffmpeg_available:
     __all__.append('VideoConverter')
